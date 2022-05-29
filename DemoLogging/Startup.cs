10eac06a2f8baf9bb3c4bf07d6c1dc73458f8102
Quoc Anh <le.quocanhh21@gmail.com>
2022-05-30 00:24:40 +0700

    add model, config, httpclient

--- DemoLogging/Startup.cs
+++ DemoLogging/Startup.cs
@@ -23,6 +23,7 @@ namespace DemoLogging
             services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
